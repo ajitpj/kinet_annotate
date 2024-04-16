375ccaa050aa05cf908b98cf1d4b9bd45028dd24
ajitpj <ajitpj@gmail.com>
2024-04-15 21:53:27 -0400

    current

--- analyze_data.py
+++ analyze_data.py
@@ -8,9 +8,9 @@ from kinet_annotate_GUI import roi_annotate
 
 
 
-parent_dir = Path('/Volumes/CDB-joglekar-lab/Juan Orozco/Data/4) Time-Lapse imaging data/20210929_KT Mitotic behavior/KI-KO experiments/')
-data_dir = parent_dir / '20240301' #'Analysis' #/ 
-
+# parent_dir = Path('/Volumes/CDB-joglekar-lab/Juan Orozco/Data/4) Time-Lapse imaging data/20210929_KT Mitotic behavior/KI-KO experiments/')
+# data_dir = parent_dir / '20240301' #'Analysis' #/ 
+data_dir = Path('/Users/ajitj/Desktop')
 files = list(data_dir.glob('*.nd2'))
 #
 viewer=napari.Viewer()
